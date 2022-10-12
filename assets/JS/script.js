42c3a15d1b965900461bb39c194aca4fabdf9498
Stanghi <stangherlin.matti@gmail.com>
2022-10-12 16:32:30 +0200

    [add] js comment

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,58 +1,48 @@
 const tariffaKm = 0.21;
-const maggioreEta = 18;
-const overEta = 65;
 const scontoVenti = 20;
 const scontoQuaranta = 40;
 let sconto;
 let prezzoFinale;
 
+// BOTTONE "GENERA"
 const btnInput = document.getElementById('btn-input');
 btnInput.addEventListener('click', function(){
-    
+
+    //INPUT-OUTPUT NOME UTENTE
     const name = document.getElementById('nome-utente').value;   
     document.getElementById('nome-utente').value = name;
-    
+    //INPUT-OUTPUT DISTANZA DA PERCORRERE
     const distanza = document.getElementById('distanza').value;    
     document.getElementById('distanza').value = distanza;
-    
-    const etaUtente = document.getElementById('eta-utente').value; 
-    document.getElementById('eta-utente').value = etaUtente;
-    
+    //INPUT ETA UTENTE
+    const etaUtente = document.getElementById('eta-utente').value;
+
+    //CALCOLO COSTO IN BASE ALLA DISTANZA
     const prezzoBiglietto = tariffaKm * distanza;
 
-    if (etaUtente < maggioreEta){
+    // CALCOLO BIGLIETTO PER MINORENNE
+    if (etaUtente === 'Minorenne'){
         sconto = (prezzoBiglietto / 100) * scontoVenti;
         prezzoFinale = prezzoBiglietto - sconto;
         document.getElementById('tipo-biglietto').innerHTML += 'Sconto minorenni';
     }
-
-    else if (etaUtente >= overEta ){
+    // CALCOLO BIGLIETTO PER OVER 65
+    else if (etaUtente === 'Over 65'){
         sconto = (prezzoBiglietto / 100) * scontoQuaranta;
         prezzoFinale = prezzoBiglietto - sconto;
         document.getElementById('tipo-biglietto').innerHTML += 'Sconto over 65';
     }
-
+    // PREZZO STANDARD PER MAGGIORENNI
     else{
         prezzoFinale = prezzoBiglietto
         document.getElementById('tipo-biglietto').innerHTML += 'Biglietto standard';
     }
-
-
+    
+    // OUTPUT NOME UTENTE SU TICKET
     document.getElementById('nome-utene').innerHTML += name;
 
+    // OUTPUT PREZZO FIXED SU TICKET
     let prezzoFinaleFixed = prezzoFinale.toFixed(2);
     let outputPrezzoFinale = `<strong class="color-output">${prezzoFinaleFixed} €<strong>`
     document.getElementById('prezzo').innerHTML += outputPrezzoFinale;
-
-
-    
 });
-
-
-
-
-
-
-
-
-
